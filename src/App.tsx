95133e927512d859234d075bc9edf86c1021131c
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2026-06-03 08:50:43 -0500

    updated registry link

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 
 import { Layout } from './pages/layout';
 import Home from './pages/home';
-// import OnCall from './pages/on-call';
 import MealTrain from './pages/meal-train';
 import SignUp from './pages/sign-up';
-// import YardWork from './pages/yard-work';
 import Registry from './pages/registry';
 import Error from './pages/error';
+// import OnCall from './pages/on-call';
+// import YardWork from './pages/yard-work';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' errorElement={<Error />}>
             <Route path='/' element={<Layout />}>
                 <Route path='/' element={<Home />} />
-                {/* <Route path='/on-call' element={<OnCall />} /> */}
                 <Route path='/meal-train' element={<MealTrain />} />
-                {/* <Route path='/yard-work' element={<YardWork />} /> */}
                 <Route path='/registry' element={<Registry />} />
                 <Route path='/sign-up' element={<SignUp />} />
+                {/* <Route path='/on-call' element={<OnCall />} /> */}
+                {/* <Route path='/yard-work' element={<YardWork />} /> */}
             </Route>
         </Route>
     )
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 export const config = {
-    registryUrl: 'https://www.amazon.com/baby-reg/brainna-langness-august-2024-saintcharles/10GFEYPIMMBCY',
+    registryUrl: 'https://www.amazon.com/baby-reg/brianna-langness-october-2026-saintpeters/34H3GHBCOVSVL',
     diaperUrl: 'https://everylife.com/collections/diaper-collection/products/everylife-diapers?variant=50896563405080',
     calendar: {
-        dueDate: new Date(2026, 9, 3),
+        dueDate: new Date(2026, 9, 2),
         startDate: new Date(2026, 8, 13),
         endDate: new Date(2026, 9, 24),
     },
