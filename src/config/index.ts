0bd1c305d95d89136d31ef989cc93042b8fee114
Nehemiah Langness <32148982+Nehemiah-Langness@users.noreply.github.com>
2026-09-20 19:33:01 -0500

    updated meal train page

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 export const config = {
     registryUrl: 'https://www.amazon.com/baby-reg/brianna-langness-october-2026-saintpeters/34H3GHBCOVSVL',
     diaperUrl: 'https://everylife.com/collections/diaper-collection/products/everylife-diapers?variant=50896563405080',
+    mealTrainUrl: "https://www.mealtrain.com/trains/q96n1r",
     calendar: {
         dueDate: new Date(2026, 9, 2),
         startDate: new Date(2026, 8, 13),
--- src/pages/meal-train.tsx
+++ src/pages/meal-train.tsx
@@ -6,6 +6,8 @@ import { SignUp } from '../components/sign-up';
 import { config } from '../config';
 import { NavbarComponent } from '../components/navbar';
 import { PageHeader } from '../components/page-header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBurger } from '@fortawesome/free-solid-svg-icons';
 
 export default function MealTrain() {
     return (
@@ -13,31 +15,57 @@ export default function MealTrain() {
             <div className='opacity-0'>
                 <NavbarComponent />
             </div>
-            <div className='d-flex flex-column align-items-center py-5'>
-                <PageHeader>Meal Train</PageHeader>
-                <DetailSection>
-                    <DetailText>If you'd like to bring us meals, let us know and we'll add you to a list to contact once the baby is born.</DetailText>
-                    <DetailText>We have no dietary restrictions and aren't picky, so whatever food you would like to bless us with, we will be grateful for.</DetailText>
-                    <DetailText>Feel free to cook or order delivery - whatever your preference.</DetailText>
-                </DetailSection>
-            </div>
-            <div className='bg-white'>
-                <div className='container py-5'>
-                    <SignUp />
-                </div>
-            </div>
-            <div className='bg-light'>
-                <div className='container py-5'>
-                    <DetailSection>
-                        <DetailText>
-                            Below, you will find a calendar with any dates that have already been reserved and you can let us know any open dates that you would prefer.
-                        </DetailText>
-                        <div className='my-5'>
-                            <Calendar startDate={config.calendar.startDate} endDate={config.calendar.endDate} dueDate={config.calendar.dueDate} volunteers={mealTrain} />
+            {config.mealTrainUrl ? (
+                <>
+                    <div className='d-flex flex-column align-items-center py-5'>
+                        <PageHeader>Meal Train</PageHeader>
+                        <DetailSection>
+                            <DetailText>You can see what days we have someone bringing us food already and sign up for a date below.</DetailText>
+                        </DetailSection>
+                    </div>
+
+                    <div className='d-flex justify-content-center gap-5'>
+                        <a
+                            className='btn btn-dark rounded-circle d-flex flex-column justify-content-center align-items-center'
+                            style={{ width: '15rem', height: '15rem' }}
+                            href={config.mealTrainUrl}
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <FontAwesomeIcon style={{ fontSize: '8rem', marginBottom: '1rem' }} icon={faBurger} />
+                            Meal Train
+                        </a>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className='d-flex flex-column align-items-center py-5'>
+                        <PageHeader>Meal Train</PageHeader>
+                        <DetailSection>
+                            <DetailText>If you'd like to bring us meals, let us know and we'll add you to a list to contact once the baby is born.</DetailText>
+                            <DetailText>We have no dietary restrictions and aren't picky, so whatever food you would like to bless us with, we will be grateful for.</DetailText>
+                            <DetailText>Feel free to cook or order delivery - whatever your preference.</DetailText>
+                        </DetailSection>
+                    </div>
+                    <div className='bg-white'>
+                        <div className='container py-5'>
+                            <SignUp />
                         </div>
-                    </DetailSection>
-                </div>
-            </div>
+                    </div>
+                    <div className='bg-light'>
+                        <div className='container py-5'>
+                            <DetailSection>
+                                <DetailText>
+                                    Below, you will find a calendar with any dates that have already been reserved and you can let us know any open dates that you would prefer.
+                                </DetailText>
+                                <div className='my-5'>
+                                    <Calendar startDate={config.calendar.startDate} endDate={config.calendar.endDate} dueDate={config.calendar.dueDate} volunteers={mealTrain} />
+                                </div>
+                            </DetailSection>
+                        </div>
+                    </div>
+                </>
+            )}
         </>
     );
 }
